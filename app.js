ba8378a41031b1cd4ccf7e0150dbdd062fe4a994
iasonasxrist <80000902+iasonasxrist@users.noreply.github.com>
2023-10-14 17:32:54 +0300

    Homework

--- app.js
+++ app.js
@@ -4,20 +4,34 @@ let weather = {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
-        "&units=metric&appid" +
+        "&units=metric&appid=" +
         this.apikey
     )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found");
-          throw new Error("No weather found!");
-        }
-        return response.json();
-      })
-      .catch((err) => {
-        console.error("Test", err);
+      .then((response) => response.json())
+      .then((data) => {
+        this.displayWeather(data);
+        console.log("🚀 ~ file: app.js:13 ~ .then ~ data:", data);
       });
   },
-};
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather; // Weather data is an array
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(
+      `City: ${name}\nWeather: ${description}\nTemperature: ${temp}°C\nHumidity: ${humidity}%\nWind Speed: ${speed} m/s`
+    );
 
+    document.querySelector(".city").innerText = `Weather in the ${name}`;
+    document.querySelector(".icon").innerText =
+      "https://openweather.org/img/wn/" + icon + ".png";
+
+    document.querySelector(".description").text = description;
+    document.querySelector(".temp").innerText = temp + "C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "" + "%";
+
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+  },
+};
 weather.fetchWeather("athens");
